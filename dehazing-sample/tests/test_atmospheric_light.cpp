62a980742dd102c62d99ea94786dde1a7a8ac669
Kentaro Iwamoto <kentaro.iwamoto.ac1026@gmail.com>
2021-11-21 23:12:23 +0900

    Add atmospheric light test

---
+++ dehazing-sample/tests/test_atmospheric_light.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+#include "atmospheric_light.hpp"
+
+TEST(AtmosphericLightTest, AtmosphericLight) {
+    // Path
+    const std::string inputImagePath = "data/original.png";
+    const std::string darkChannelImagePath = "data/dark_channel.png";
+
+    // Read images
+    const cv::Mat inputImage = cv::imread(inputImagePath);
+    const cv::Mat darkChannelImage = cv::imread(darkChannelImagePath, cv::IMREAD_GRAYSCALE);
+    cv::Mat outputImage;
+
+    // Execute
+    const double candidateAreaRate = 0.001;
+    cv::Vec3b atmosphericLight;
+    AtmosphericLight *atmosphericLightEstimator = new AtmosphericLight(candidateAreaRate);
+    atmosphericLightEstimator->execute(inputImage, darkChannelImage, atmosphericLight);
+
+    EXPECT_EQ((int)(unsigned char)atmosphericLight[0], 255);
+    EXPECT_EQ((int)(unsigned char)atmosphericLight[1], 252);
+    EXPECT_EQ((int)(unsigned char)atmosphericLight[2], 249);
+}
