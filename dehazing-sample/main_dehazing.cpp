3b29ec9b459d8f7d8280a3b597c300c2ab1b77e1
Kentaro Iwamoto <kentaro.iwamoto.ac1026@gmail.com>
2021-11-23 11:56:23 +0900

    Fix to execute dehazing by main function

--- dehazing-sample/main_dehazing.cpp
+++ dehazing-sample/main_dehazing.cpp
@@ -3,6 +3,9 @@
 #include <opencv2/opencv.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include "dehazing.hpp"
 
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
@@ -11,16 +14,18 @@ int main(int argc, char **argv) {
     po::positional_options_description positonalOptions;
     po::options_description allOptions("input/output options");
     po::variables_map values;
-    std::string inputImageDir, outputImageDir;
+    std::string inputImageDir, outputImageDir, parameterJson;
 
     // Comanndline option
     try {
         positonalOptions.add("InputImageDir", 1)
-            .add("OutputImageDir", 1);
+            .add("OutputImageDir", 1)
+            .add("ParameterJson", 1);
         allOptions.add_options()
             ("help,h", "Show help")
             ("InputImageDir", po::value<std::string>(&inputImageDir)->required(), "Path to input image directory")
-            ("OutputImageDir", po::value<std::string>(&outputImageDir)->required(), "Path to output image directory");
+            ("OutputImageDir", po::value<std::string>(&outputImageDir)->required(), "Path to output image directory")
+            ("ParameterJson", po::value<std::string>(&parameterJson)->required(), "Path to parameter json file");
 
         po::store(
             po::command_line_parser{argc, argv}
@@ -58,6 +63,25 @@ int main(int argc, char **argv) {
         }
     }
 
+    // If not exist parameter json file
+    if (!fs::exists(parameterJson)) {
+        std::cout << "Directory " << parameterJson << " is not found." << std::endl;
+        return 1;
+    }
+
+    // Read parameters
+    boost::property_tree::ptree parameters;
+    boost::property_tree::read_json(parameterJson, parameters);
+    const double candidateAreaRate = parameters.get<double>("candidateAreaRate");
+    const int darkChannelNeighborRadius = parameters.get<int>("darkChannelNeighborRadius");
+    const double minTransmission = parameters.get<double>("minTransmission");
+    const double omega = parameters.get<double>("omega");
+    const int guidedFilterRadius = parameters.get<int>("guidedFilterRadius");
+    const double guidedFilterEps = parameters.get<double>("guidedFilterEps");
+
+    // Initialize dehazing object
+    Dehazing *dehazing = new Dehazing(candidateAreaRate, darkChannelNeighborRadius, minTransmission, omega, guidedFilterRadius, guidedFilterEps);
+
     // Scan all files in inputImageDir
     const fs::directory_iterator inputImageFiles= fs::directory_iterator(fs::path(inputImageDir));
     for (const fs::directory_entry& inputFile: inputImageFiles) {
@@ -68,10 +92,15 @@ int main(int argc, char **argv) {
         const cv::Mat inputImage = cv::imread(inputFilePathStr);
         std::cout << filename.string() << std::endl;
 
+        cv::Mat outputImage;
+        dehazing->execute(inputImage, outputImage);
+
         // output
         const std::string outputFilePathStr = fs::path(outputImageDirPath / filename).string();
-        cv::imwrite(outputFilePathStr, inputImage);
+        cv::imwrite(outputFilePathStr, outputImage);
     }
+
+    delete dehazing;
 
     return 0;
 }
