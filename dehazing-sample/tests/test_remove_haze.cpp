5b03a8f1f9ded2b572b4b30d04db797ba9d56bf1
ken1714 <2773566-ken1714@users.noreply.gitlab.com>
2021-11-23 10:01:06 +0900

    Implement removing haze

---
+++ dehazing-sample/tests/test_remove_haze.cpp
@@ -0,0 +1,25 @@
+#include "gtest/gtest.h"
+#include "remove_haze.hpp"
+#include "utils.hpp"
+
+TEST(RemoveHazeTest, RemoveHaze) {
+    // Path
+    const std::string inputImagePath = "data/original.png";
+    const std::string transmissionPath = "data/transmission_map.png";
+    const std::string referenceImagePath = "data/dehazed.png";
+
+
+    // Read images
+    const cv::Mat inputImage = cv::imread(inputImagePath);
+    const cv::Mat transmissionMap = cv::imread(transmissionPath);
+    const cv::Mat referenceImage = cv::imread(referenceImagePath);
+    cv::Mat outputImage;
+
+    // Execute
+    const double minTransmission = 0.1;
+    const cv::Vec3b atmosphericLight = cv::Vec3b(255, 252, 249);
+    RemoveHaze *removeHaze = new RemoveHaze(inputImage.size(), minTransmission);
+    removeHaze->execute(inputImage, transmissionMap, atmosphericLight, outputImage);
+
+    EXPECT_EQ(equal2Images(outputImage, referenceImage), true);
+}
