7c8262fa0185fb5d2f321ca0454e477162551036
ken1714 <2773566-ken1714@users.noreply.gitlab.com>
2021-11-23 10:16:24 +0900

    Remove imageSize from ImageProcess class

--- dehazing-sample/include/dark_channel_prior.hpp
+++ dehazing-sample/include/dark_channel_prior.hpp
@@ -4,7 +4,7 @@
 
 class DarkChannelPrior: public ImageProcess {
     public:
-        DarkChannelPrior(const cv::Size imageSize, const int neighborRadius);
+        DarkChannelPrior(const int neighborRadius);
         void execute(const cv::Mat& inputImage, cv::Mat& outputImage);
 
     protected:
--- dehazing-sample/include/remove_haze.hpp
+++ dehazing-sample/include/remove_haze.hpp
@@ -4,7 +4,7 @@
 
 class RemoveHaze: public ImageProcess {
     public:
-        RemoveHaze(const cv::Size imageSize, const double minTransmission);
+        RemoveHaze(const double minTransmission);
         void execute(const cv::Mat& inputImage, const cv::Mat& transmissionMap, const cv::Vec3b atmosphericLight, cv::Mat& outputImage);
 
     protected:
